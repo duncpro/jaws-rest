0ad3382bf3d4ab6b89fbf1f0b46da278c783ddc1
Duncan Proctor <dbp19a@my.fsu.edu>
2021-10-21 11:16:15 -0400

    add function prototype

--- web-ui/build.gradle.kts
+++ web-ui/build.gradle.kts
@@ -3,3 +3,7 @@ val dtoInterfaces: Configuration by configurations.creating {}
 dependencies {
     dtoInterfaces(project(":request-handler-lambda", "dtoInterfaces"))
 }
+
+fun buildLocalNodeModule(dir: File) {
+
+}
